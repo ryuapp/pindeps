47021937edfbb5dbc60697d61b7faaa1baf47fb2
ryu <114303361+ryuapp@users.noreply.github.com>
2025-09-19 00:41:00 +0900

    ci: fix build script

--- scripts/build-package.ts
+++ scripts/build-package.ts
@@ -1,5 +1,5 @@
 #!/usr/bin/env -S deno -W=bin --unstable
-await Deno.remove("./bin");
+await Deno.remove("./bin").catch(() => {});
 await Deno.bundle({
   entrypoints: ["./src/main.ts"],
   outputPath: "./bin",
