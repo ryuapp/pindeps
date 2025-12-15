f377fbbe795953188dd693008e944f0b70fe2f7b
ryu <114303361+ryuapp@users.noreply.github.com>
2025-12-16 02:36:15 +0900

    fix: use deno namespace for deno (#34)

--- src/main.ts
+++ src/main.ts
@@ -3,19 +3,27 @@
 import { parseArgs } from "@std/cli/parse-args";
 import packageJson from "../package.json" with { type: "json" };
 import { runPinCommand } from "./commands/pin.ts";
-import process from "node:process";
+
+function isDeno(): boolean {
+  return globalThis.navigator?.userAgent?.includes("Deno");
+}
+
+function exit(code: number): void {
+  isDeno() ? Deno.exit(code) : globalThis.process.exit(code);
+}
 
 function main(): void {
-  const args = parseArgs(process.argv.slice(2), {
+  const inputArgs = isDeno() ? Deno.args : globalThis.process.argv.slice(2);
+  const args = parseArgs(inputArgs, {
     boolean: ["version", "dev"],
   });
   if (args.version) {
     console.log(`pindeps ${packageJson.version}`);
-    process.exit(0);
+    exit(0);
   }
 
   const exitCode = runPinCommand({ dev: args.dev });
-  process.exit(exitCode);
+  exit(exitCode);
 }
 
 main();
