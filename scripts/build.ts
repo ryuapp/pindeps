08cce4321d00ffd92babda15eb9ae62871b33bcc
Ryu <114303361+ryuapp@users.noreply.github.com>
2026-06-03 01:47:26 +0900

    chore: use tsdown (#59)
    * chore: use tsdown
    
    * fmt

---
+++ scripts/build.ts
@@ -0,0 +1,20 @@
+#!/usr/bin/env -S deno run -WRES --allow-ffi scripts/build.ts
+import denoPlugin from "@deno/rolldown-plugin";
+import { build } from "tsdown";
+
+await Deno.remove("./bin").catch(() => {});
+
+await build({
+  config: false,
+  entry: {
+    bin: "./src/main.ts",
+  },
+  format: "esm",
+  target: false,
+  clean: false,
+  outDir: ".",
+  plugins: [denoPlugin()],
+  outputOptions: {
+    entryFileNames: "bin",
+  },
+});
